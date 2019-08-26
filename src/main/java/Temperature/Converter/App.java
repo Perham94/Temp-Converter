ddf36c514031f85cbfd26dd8dfe53b1ae84cb310
Perham <perham132@hotmail.se>
2019-08-26 14:38:14 +0200

    start up

---
+++ src/main/java/Temperature/Converter/App.java
@@ -0,0 +1,13 @@
+package Temperature.Converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
