badccdf3308ac732031481b3a573bced8dceb314
Xarczuz <chjunchi.chiu@gmail.com>
2019-08-26 15:42:27 +0200

    Created temperature class

--- src/main/java/Temperature/Converter/Converter.java
+++ src/main/java/Temperature/Converter/Converter.java
@@ -1,5 +1,7 @@
 package Temperature.Converter;
 
 public class Converter {
+	
 
+	
 }
--- src/main/java/Temperature/Converter/Temperature.java
+++ src/main/java/Temperature/Converter/Temperature.java
@@ -1,5 +1,23 @@
 package Temperature.Converter;
 
 public class Temperature {
+	private String tempUnit;
+	private double temp;
+
+	public String getTempUnit() {
+		return tempUnit;
+	}
+
+	public void setTempUnit(String tempUnit) {
+		this.tempUnit = tempUnit;
+	}
+
+	public double getTemp() {
+		return temp;
+	}
+
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
 
 }
