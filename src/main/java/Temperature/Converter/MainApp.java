172614dc514506f7b0fdf1cb541d1cfcbc156f93
Xarczuz <chjunchi.chiu@gmail.com>
2019-09-08 19:53:21 +0200

    CC JAvaFX

--- src/main/java/Temperature/Converter/MainApp.java
+++ src/main/java/Temperature/Converter/MainApp.java
@@ -3,10 +3,9 @@ package Temperature.Converter;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
-import javafx.geometry.Rectangle2D;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -14,20 +13,14 @@ public class MainApp extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-		primaryStage.setX(primaryScreenBounds.getMinX());
-		primaryStage.setY(primaryScreenBounds.getMinY());
-		primaryStage.setWidth(1366);
-		primaryStage.setHeight(765);
-		primaryStage.setTitle("SolKlart Väderprognos");
-		BorderPane border = new BorderPane();
-
-		Scene scene = new Scene(border);
+		Parent root = FXMLLoader.load(getClass().getResource("MainMeny.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("SolKlart Väder");
+		scene.getStylesheets().add("/resource/CSS/MainMeny.css");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
