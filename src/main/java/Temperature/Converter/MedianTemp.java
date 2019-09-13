dc80fb460f4d1673a805df4fb927b88b010327e9
Johan <johanjames@hotmail.com>
2019-09-13 11:01:15 +0200

    Ny metod för median. Ny klass för sortering av templist i stigande ordning.

--- src/main/java/Temperature/Converter/MedianTemp.java
+++ src/main/java/Temperature/Converter/MedianTemp.java
@@ -27,12 +27,27 @@ public class MedianTemp {
 		return TempList.get(Math.round(result)).getTemp() + TempList.get(Math.round(result)).getTempUnit();
 	}
 	
-	public Double getMedian() {
+	public double getMedian() {
+		ArrayList<Temperature> sortedList = SortTempList.sortList(TempList);
 		Double median = 0.0;
 		result = num_elements % 2;
-		System.out.println("result: " + result);
-		//return median;
+		
+		if(result == 0) { // Om jämnt antal mätvärden
+
+			int indexMedianLow = (num_elements/2) - 1;
+			int indexMedianHigh = (num_elements/2);
+			
+			double medianLow = sortedList.get(indexMedianLow).getTemp();
+			double medianHigh = sortedList.get(indexMedianHigh).getTemp();
+			
+			median = (medianHigh + medianLow) / 2;
+		}
+		else { // Om udda antal mätvärden
+			
+			int indexMedian = (int) Math.floor(num_elements/2);
+			median = sortedList.get(indexMedian).getTemp();
+		}
+		
 		return median;
 	}
-
 }
--- src/main/java/Temperature/Converter/SortTempList.java
+++ src/main/java/Temperature/Converter/SortTempList.java
@@ -1,5 +1,39 @@
 package Temperature.Converter;
 
-public class SortTempList {
+import java.util.ArrayList;
 
+public class SortTempList {
+	
+	public SortTempList() {	}
+
+	// Sorterar mätvärden i stigande ordning
+	public static ArrayList<Temperature> sortList(ArrayList<Temperature> unsortedList) {
+			ArrayList<Temperature> sortedList = new ArrayList<>();
+			
+			// söker igenom $unsortedList $i antal gånger
+			int iterationCount = unsortedList.size();
+			for(int i = 0; i < iterationCount; i++) {
+				double minTemp = 0.0;
+				int minTempIndex = 0;
+				// söker igenom $unsortedList efter minsta värde
+				for(int j = 0; j < unsortedList.size(); j++) {
+					if(j == 0) { // första iterationen?
+						minTemp = unsortedList.get(0).getTemp();
+						minTempIndex = 0;
+					}
+					else if(unsortedList.get(j).getTemp() < minTemp) {					
+						minTemp = unsortedList.get(j).getTemp();
+						minTempIndex = j;
+					}				
+				}				
+				unsortedList.remove(minTempIndex); // värdet som sorterats ut raderas från lista
+				
+				// lägger till minsta värdet sist i $sortedList
+				Temperature tempObj = new Temperature();
+				tempObj.setTemp(minTemp);
+				sortedList.add(tempObj);
+			}
+					
+			return sortedList;
+		}	
 }
