76dbe6b65e6bf1df0f82b846077553a161a5abb7
Johan <johanjames@hotmail.com>
2019-09-13 11:00:34 +0200

    Kompletterande metod för medianvärde. Ny klass som sorterar templista i stigande ordning.

--- src/main/java/Temperature/Converter/MedianTemp.java
+++ src/main/java/Temperature/Converter/MedianTemp.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class MedianTemp {
 	private ArrayList<Temperature> TempList;
 	private int result;
+	private int num_elements;
 
 	/**
 	 * @param tempList
@@ -15,6 +16,7 @@ public class MedianTemp {
 	public MedianTemp(ArrayList<Temperature> tempList) {
 
 		TempList = new ArrayList<Temperature>(tempList);
+		num_elements = TempList.size();
 
 	}
 
@@ -23,6 +25,14 @@ public class MedianTemp {
 		result = TempList.size() / 2;
 
 		return TempList.get(Math.round(result)).getTemp() + TempList.get(Math.round(result)).getTempUnit();
+	}
+	
+	public Double getMedian() {
+		Double median = 0.0;
+		result = num_elements % 2;
+		System.out.println("result: " + result);
+		//return median;
+		return median;
 	}
 
 }
